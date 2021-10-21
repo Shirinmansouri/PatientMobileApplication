bbf65469d3fcae3ccd1dc3d23bcd4a96a4f5fe95
Shirin <shirin.mansori@gmail.com>
2021-10-21 16:42:04 -0400

    Change menu

--- App.js
+++ App.js
@@ -1,32 +1,48 @@
 import React, { Component } from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import PatientsScreen from './PatientsScreen'
 import RecordsScreen from './RecordsScreen'
 import Icon  from 'react-native-vector-icons/Ionicons'
 Icon.loadFont()
 
-const Tab=createBottomTabNavigator();
+const Tab=createMaterialTopTabNavigator();
 
 function App ()
 {
   return (
   <NavigationContainer>
-  <Tab.Navigator screenOptions={({route})=>({
+  <Tab.Navigator
+
+
+
+  screenOptions={{
+    tabBarLabelStyle: { fontSize: 40 },
+    tabBarItemStyle: { width: 100 },
+    tabBarStyle: { backgroundColor: 'powderblue' }},
+
+    ({route})=>({
+
     tabBarIcon : ({focused,size,color}) => {
       let iconName;
-      if(route.name=== 'PatientsScreen'){
-        iconName="bed-patient";
+      if(route.name=== 'Patients'){
+        iconName="people";
+        size= focused? 25 :20;
       }
-      else if (route.name==='RecordsScreen')
+      else if (route.name==='Records')
       {
         iconName="list";
+            size= focused? 25 :20;
       }
       return (
-            <Icon  size={30} name={iconName}/>
+            <Icon  size={size} name={iconName}/>
         );
     }
-    })}>
+    })
+  }
+    >
   <Tab.Screen
     name ="Patients"
     component={PatientsScreen}
